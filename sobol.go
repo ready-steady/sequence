bbe78622eab3e91d00fd29e09eb2b5cfd18da3ee
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-23 07:46:32 +0200

    Make Sobol sequential

--- sobol.go
+++ sobol.go
@@ -1,30 +1,53 @@
 package sequence
 
-// Sobol generates a Sobol sequence. The maximal number of dimensions is 21201,
+// Sobol represents a Sobol sequence. The maximal number of dimensions is 21201,
 // and the maximal number of points is 2^32.
 //
 // https://en.wikipedia.org/wiki/Sobol_sequence
-func Sobol(dimensions, points uint, seed int64) []float64 {
+type Sobol struct {
+	dimensions uint
+	offset     uint
+	cursor     []uint32
+}
+
+// NewSobol returns a new Sobol sequence.
+func NewSobol(dimensions uint, scramble int64) *Sobol {
+	cursor := make([]uint32, dimensions)
+	for i := range cursor {
+		cursor[i] = uint32(scramble)
+	}
+
+	return &Sobol{
+		dimensions: dimensions,
+		cursor:     cursor,
+	}
+}
+
+// Next advances the sequence and returns the traversed points.
+func (s *Sobol) Next(points uint) []float64 {
 	const (
 		bits = 32
 		size = 1 << bits
 	)
 
+	dimensions, offset, cursor := s.dimensions, s.offset, s.cursor
+
 	index := make([]uint, points)
-	for i := uint(1); i < points; i++ {
-		for j := i - 1; j&1 != 0; j >>= 1 {
+	for i := uint(0); i < points; i++ {
+		for j := offset + i; j&1 != 0; j >>= 1 {
 			index[i]++
 		}
 	}
 
 	data := make([]float64, points*dimensions)
 	for i := uint(0); i < dimensions; i++ {
-		data[i] = float64(uint32(seed)) / size
-		for j, x := uint(1), uint32(seed); j < points; j++ {
-			x ^= sobolData[i*bits+index[j]]
-			data[j*dimensions+i] = float64(x) / size
+		for j := uint(0); j < points; j++ {
+			data[j*dimensions+i] = float64(cursor[i]) / size
+			cursor[i] ^= sobolData[i*bits+index[j]]
 		}
 	}
+
+	s.offset += points
 
 	return data
 }
--- sobol_test.go
+++ sobol_test.go
@@ -6,8 +6,39 @@ import (
 	"github.com/ready-steady/assert"
 )
 
-func TestSobol(t *testing.T) {
-	data := []float64{
+func TestSobol3D(t *testing.T) {
+	sequence := NewSobol(3, 0)
+
+	actual := append(append([]float64{},
+		sequence.Next(5)...),
+		sequence.Next(5)...)
+
+	expected := []float64{
+		0.0000, 0.0000, 0.0000,
+		0.5000, 0.5000, 0.5000,
+		0.7500, 0.2500, 0.2500,
+		0.2500, 0.7500, 0.7500,
+		0.3750, 0.3750, 0.6250,
+		0.8750, 0.8750, 0.1250,
+		0.6250, 0.1250, 0.8750,
+		0.1250, 0.6250, 0.3750,
+		0.1875, 0.3125, 0.9375,
+		0.6875, 0.8125, 0.4375,
+	}
+
+	assert.Equal(actual, expected, t)
+}
+
+func TestSobol10D(t *testing.T) {
+	sequence := NewSobol(10, 0)
+
+	actual := append(append(append(append([]float64{},
+		sequence.Next(1)...),
+		sequence.Next(2)...),
+		sequence.Next(3)...),
+		sequence.Next(4)...)
+
+	expected := []float64{
 		0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
 		0.5000, 0.5000, 0.5000, 0.5000, 0.5000, 0.5000, 0.5000, 0.5000, 0.5000, 0.5000,
 		0.7500, 0.2500, 0.2500, 0.2500, 0.7500, 0.7500, 0.2500, 0.7500, 0.7500, 0.7500,
@@ -20,5 +51,5 @@ func TestSobol(t *testing.T) {
 		0.6875, 0.8125, 0.4375, 0.9375, 0.0625, 0.8125, 0.9375, 0.4375, 0.4375, 0.8125,
 	}
 
-	assert.Equal(Sobol(10, 10, 0), data, t)
+	assert.Equal(actual, expected, t)
 }
